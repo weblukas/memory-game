c2982ab256f87b9c94a100818fe05f522ea22a9c
Łukasz Szmyd <szmydlukasz30@gmail.com>
2020-08-12 20:11:45 +0200

    bug fixed

--- app.js
+++ app.js
@@ -1,50 +1,44 @@
 
-      const cards = ['cow-1', 'cow-2', 'eleph-1', 'eleph-2', 'lamb-1', 'lamb-2', 'penguin-1', 'penguin-2', 'rat-1', 'rat-2', 'shark-1', 'shark-2']
-      const allCards = Array.from(document.querySelectorAll('.card'));
-      // w sumie nie użyłem tej tablicy, później użyje 
-      let checkedCardPic = false;
-      let disableClick = false;
-      let firstCard, secondCard;
-      
-      
-      function matchCards(){
-        if(firstCard.getAttribute('data-pic') === secondCard.getAttribute('data-pic')){
-          
-        }else {
-          disableClick = true;
-          setTimeout(function(){
-            firstCard.classList.toggle('hidden');
-            secondCard.classList.toggle('hidden');
-          }, 500)
-          disableClick = false;
-          
-        }
-      }
+const allCards = Array.from(document.querySelectorAll(".card"));
+// w sumie nie użyłem tej tablicy, później użyje
+let alreadyClicked = false;
+let disableClick = false;
+let firstCard, secondCard;
 
-      allCards.forEach(el=>{
-          el.addEventListener('click', checkCard)})
-          
-          
-          function checkCard(){
-            
-            if(disableClick){
-              return;
-            }
-            if(!checkedCardPic) {
-              checkedCardPic = true;
-              
-              firstCard = this;
-              this.classList.toggle('hidden')
-              
-            }else {
-              checkedCardPic = false;
-              disableClick = true;
-              secondCard = this;
-              this.classList.toggle('hidden')
-              matchCards();
-              // allCards.forEach(el => el.removeEventListener('click', checkCard))  
-            }
-              // allCards.forEach(el => el.addEventListener('click', checkCard)) 
-          }
+function matchCards() {
+  if (
+    firstCard.getAttribute("data-pic") === secondCard.getAttribute("data-pic")
+  ) {
+    // disableClick = false;
+  } else {
+    disableClick = true;
+    setTimeout(function () {
+      firstCard.classList.add("hidden");
+      secondCard.classList.add("hidden");
+      disableClick = false;
+    }, 500);
+  }
+}
 
-        
+allCards.forEach((el) => {
+  el.addEventListener("click", checkCard);
+});
+
+function checkCard() {
+  if (disableClick) {
+    return;
+  }
+  if (!alreadyClicked) {
+    alreadyClicked = true;
+    firstCard = this;
+    firstCard.classList.remove("hidden");
+  } else {
+    alreadyClicked = false;
+    disableClick = true;
+    secondCard = this;
+    this.classList.remove("hidden");
+    matchCards();
+    // allCards.forEach(el => el.removeEventListener('click', checkCard))
+  }
+  // allCards.forEach(el => el.addEventListener('click', checkCard))
+}
